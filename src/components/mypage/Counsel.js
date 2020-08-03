41a59d08b0b9406cffe6323f05260fd246b35867
100seungho <pouy1015@snu.ac.kr>
2020-08-03 21:02:33 +0900

    edited few layout

--- src/components/mypage/Counsel.js
+++ src/components/mypage/Counsel.js
@@ -79,33 +79,41 @@ class Counsel extends React.Component {
       ) : null;
 
     return (
-        <Card title="상담 내역">
-      <List
-        className="demo-loadmore-list"
-        loading={initLoading}
-        itemLayout="horizontal"
-        loadMore={loadMore}
-        dataSource={list}
-        renderItem={(item) => (
-          <List.Item
-            actions={[
-              <a key="list-loadmore-edit">상담 내용 다시보기</a>,
-              <a key="list-loadmore-more">리뷰 남기기</a>,
-            ]}
-          >
-            <Skeleton avatar title={false} loading={item.loading} active>
-              <List.Item.Meta
-                avatar={
-                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                }
-                title={<a href="https://ant.design">{item.name.last}</a>}
-                description={"회사명"+" | "+"직무"+" | "+"2020년 5월 입사"+" | "+"서울대학교 경영학과 졸업"}
-              />
+      <Card title="상담 내역">
+        <List
+          className="demo-loadmore-list"
+          loading={initLoading}
+          itemLayout="horizontal"
+          loadMore={loadMore}
+          dataSource={list}
+          renderItem={(item) => (
+            <List.Item
+              actions={[
+                <a key="list-loadmore-edit">상담 내용 다시보기</a>,
+                <a key="list-loadmore-more">리뷰 남기기</a>,
+              ]}
+            >
+              <Skeleton avatar title={false} loading={item.loading} active>
+                <List.Item.Meta
+                  avatar={
+                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                  }
+                  title={<a href="https://ant.design">{item.name.last}</a>}
+                  description={
+                    "회사명" +
+                    " | " +
+                    "직무" +
+                    " | " +
+                    "2020년 5월 입사" +
+                    " | " +
+                    "서울대학교 경영학과 졸업"
+                  }
+                />
                 <div>"감사합니다!"</div>
-            </Skeleton>
-          </List.Item>
-        )}
-      />
+              </Skeleton>
+            </List.Item>
+          )}
+        />
       </Card>
     );
   }
--- src/components/mypage/OneOnOne.js
+++ src/components/mypage/OneOnOne.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Form, Input, InputNumber, Button, Card, List, Typography,Space } from "antd";
+import {
+  Form,
+  Input,
+  InputNumber,
+  Button,
+  Card,
+  List,
+  Typography,
+  Space,
+} from "antd";
 
 const layout = {
   labelCol: {
@@ -14,19 +23,16 @@ const validateMessages = {
   types: {
     email: "${label} is not validate email!",
     number: "${label} is not a validate number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
-  },
+  }
 };
 
 const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires.",
+];
 
 const OneOnOne = () => {
   const onFinish = (values) => {
@@ -34,7 +40,7 @@ const OneOnOne = () => {
   };
 
   return (
-    <Space size="large" direction="vertical" style={{width:"100%"}}>
+    <Space size="large" direction="vertical" style={{ width: "100%" }}>
       <Card title="1대1 문의하기">
         <div style={{ maxWidth: 700, margin: "auto" }}>
           <Form
@@ -49,15 +55,28 @@ const OneOnOne = () => {
               rules={[
                 {
                   type: "email",
+                  message: "올바른 이메일 형식이 아닙니다!",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input placeholder="답변받으실 이메일 주소를 입력해주세요" />
+            </Form.Item>
+            <Form.Item name={["user", "introduction"]} label="문의내용">
+              <Input.TextArea placeholder="문의 내용을 적어주세요" required />
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{ ...layout.wrapperCol, offset: 0 }}
+              rules={[
+                {
+                  type: "email",
+                },
+                {
+                  required: true,
+                  message: "문의 내용을 적어주세요!",
                 },
               ]}
             >
-              <Input />
-            </Form.Item>
-            <Form.Item name={["user", "introduction"]} label="문의내용">
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 0 }}>
               <Button type="primary" htmlType="submit">
                 문의하기
               </Button>
