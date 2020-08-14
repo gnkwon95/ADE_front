298f608f991f93e12143fec94ffb9352e3c383b4
gnkwon95 <gnkwon95@gmail.com>
2020-08-14 18:05:56 +0900

    add calendar, with bug

---
+++ src/components/Mentor/handleok.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class HandleOK extends React.Component {
+    render() {
+        return (
+            <h5> pop </h5>
+        )
+    }
+}
+
+export default HandleOK;
